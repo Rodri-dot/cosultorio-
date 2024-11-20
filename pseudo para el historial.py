079dd3eb6daf82dd8ecd62ad098afbda53a340c8
Rodri-dot <mariogamerplay78@gmail.com>
2024-11-19 23:15:53 -0600

    Initial commit

---
+++ pseudo para el historial.py
@@ -0,0 +1,11 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Inicio
+...     Recuperar el nombre del paciente de localStorage
+...     Si existe el nombre
+...         Seleccionar una enfermedad aleatoria de una lista predefinida
+...         Mostrar el nombre del paciente y la enfermedad en pantalla
+...     Si no
+...         Mostrar mensaje de "No hay información disponible"
+...     Fin Si
+... Fin
---
+++ script.js
@@ -0,0 +1,7 @@
+Inicio
+    Mostrar formulario para agendar cita
+    Si el usuario completa los campos de nombre y fecha
+        Guardar el nombre en localStorage
+        Mostrar mensaje de confirmación
+    Fin Si
+Fin
